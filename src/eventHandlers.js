1cfe734fdc6315960bf647419a734ab2d260144a
Dove6 <Dove6@users.noreply.github.com>
2022-06-29 20:36:54 +0200

    Refactor event handlers

--- src/eventHandlers.js
+++ src/eventHandlers.js
@@ -1,12 +1,39 @@
+const routerSettings = [
+    {
+        enabledCheck: document.getElementById("chkFirstRouter"),
+        powerSlider: document.getElementById("sldRouter1Power"),
+        powerOut: document.getElementById("outRouter1Power"),
+        lowFreqRadio: document.getElementById("chkRouter1Low"),
+        highFreqRadio: document.getElementById("chkRouter1High"),
+        customFreqRadio: document.getElementById("chkRouter1Custom"),
+        customFreqSlider: document.getElementById("sldRouter1CustomValue"),
+        customFreqOut: document.getElementById("outRouter1CustomValue")
+    },
+    {
+        enabledCheck: document.getElementById("chkSecondRouter"),
+        powerSlider: document.getElementById("sldRouter2Power"),
+        powerOut: document.getElementById("outRouter2Power"),
+        lowFreqRadio: document.getElementById("chkRouter2Low"),
+        highFreqRadio: document.getElementById("chkRouter2High"),
+        customFreqRadio: document.getElementById("chkRouter2Custom"),
+        customFreqSlider: document.getElementById("sldRouter2CustomValue"),
+        customFreqOut: document.getElementById("outRouter2CustomValue")
+    }
+];
+const dampingInput = document.getElementById("numDamping");
+const phaseVelocityInput = document.getElementById("numPhaseVelocity");
+const refractionModifierInput = document.getElementById("numRefractionModifier");
+const tpsSlider = document.getElementById("sldTicksPerSecond");
+const tpsOut = document.getElementById("outTicksPerSecond");
+const stepsPerTickSlider = document.getElementById("sldStepsPerTick");
+const stepsPerTickOut = document.getElementById("outStepsPerTick");
+const positiveWaveColor = document.getElementById("clrPositiveWave");
+const negativeWaveColor = document.getElementById("clrNegativeWave");
+const signOnlyCheck = document.getElementById("chkSignOnly");
+
 function btnPausedEvent(btn) {
-    if (paused) {
-        paused = false;
-        btn.value = "Pause";
-    }
-    else {
-        paused = true;
-        btn.value = "Resume";
-    }
+    paused = !paused;
+    btn.value = paused ? "Resume" : "Pause";
 }
 
 function btnCleanEvent() {
@@ -14,124 +41,97 @@ function btnCleanEvent() {
 }
 
 function btnResetEvent() {
-    // change all values to default
-    routers[0].frequency = Router.LOW_FREQUENCY;
-    routers[1].frequency = Router.LOW_FREQUENCY;
-    routers[0].powerPct = 1;
-    routers[1].powerPct = 1;
-    Physics.damping = 0.25;
-    Physics.phaseVelocity = 0.2;
-    Physics.globalRefractionModifier = 2.5;
-    Physics.tps = 60;
-    Physics.stepsPerTick = 1;
-    Room.positiveWaveColor = [255, 0, 0];
-    Room.negativeWaveColor = [0, 0, 255];
-    Room.showSignOnly = false;
     // change all controls to default
-    document.getElementById("chkFirstRouter").checked = true;
-    document.getElementById("chkRouter1Low").checked = true;
-    document.getElementById("sldRouter1Power").value = 1;
-    document.getElementById("outRouter1Power").value = (1).toFixed(2);
-    document.getElementById("sldRouter2Power").value = 1;
-    document.getElementById("outRouter2Power").value = (1).toFixed(2);
-    document.getElementById("txtRouter1CustomValue").value = 0;
-    document.getElementById("chkSecondRouter").checked = false;
-    document.getElementById("chkRouter2High").checked = true;
-    document.getElementById("txtRouter2CustomValue").value = 0;
-    document.getElementById("txtDamping").value = 0.25;
-    document.getElementById("txtPhaseVelocity").value = 0.20;
-    document.getElementById("txtRefractionModifier").value = 2.5;
-    document.getElementById("sldTicksPerSecond").value = 60;
-    document.getElementById("outTicksPerSecond").value = 60;
-    document.getElementById("sldStepsPerTick").value = 1;
-    document.getElementById("outStepsPerTick").value = 1;
-    document.getElementById("clrPositiveWave").value = "#ff0000";
-    document.getElementById("clrNegativeWave").value = "#0000ff";
-    document.getElementById("chkSignOnly").checked = false;
+    routerSettings[0].enabledCheck.checked = true;
+    routerSettings[0].lowFreqRadio.checked = true;
+    routerSettings[0].powerSlider.value = routerSettings[0].powerSlider.defaultValue;
+    routerSettings[0].customFreqSlider.value = routerSettings[0].customFreqSlider.defaultValue;
+    routerSettings[1].enabledCheck.checked = false;
+    routerSettings[1].highFreqRadio.checked = true;
+    routerSettings[1].powerSlider.value = routerSettings[1].powerSlider.defaultValue;
+    routerSettings[1].customFreqSlider.value = routerSettings[1].customFreqSlider.defaultValue;
+    dampingInput.value = dampingInput.defaultValue;
+    phaseVelocityInput.value = phaseVelocityInput.defaultValue;
+    refractionModifierInput.value = refractionModifierInput.defaultValue;
+    tpsSlider.value = tpsSlider.defaultValue;
+    stepsPerTickSlider.value = stepsPerTickSlider.defaultValue;
+    positiveWaveColor.value = positiveWaveColor.defaultValue;
+    negativeWaveColor.value = negativeWaveColor.defaultValue;
+    signOnlyCheck.checked = false;
+
+    // dispatch events in order to update simulation variables
+    routerSettings[0].enabledCheck.dispatchEvent(new Event('change'));
+    routerSettings[0].lowFreqRadio.dispatchEvent(new Event('change'));
+    routerSettings[0].powerSlider.dispatchEvent(new Event('input'));
+    routerSettings[0].customFreqSlider.dispatchEvent(new Event('input'));
+    routerSettings[1].enabledCheck.dispatchEvent(new Event('change'));
+    routerSettings[1].highFreqRadio.dispatchEvent(new Event('change'));
+    routerSettings[1].powerSlider.dispatchEvent(new Event('input'));
+    routerSettings[1].customFreqSlider.dispatchEvent(new Event('input'));
+    dampingInput.dispatchEvent(new Event('change'));
+    phaseVelocityInput.dispatchEvent(new Event('change'));
+    refractionModifierInput.dispatchEvent(new Event('change'));
+    tpsSlider.dispatchEvent(new Event('input'));
+    stepsPerTickSlider.dispatchEvent(new Event('input'));
+    positiveWaveColor.dispatchEvent(new Event('input'));
+    negativeWaveColor.dispatchEvent(new Event('input'));
+    signOnlyCheck.dispatchEvent(new Event('change'));
 }
 
-function chkRouter1EnabledEvent(chk) {
-    routers[0].enabled = chk.checked;
+function chkRouterEnabledEvent(chk, index) {
+    routers[index].enabled = chk.checked;
 }
 
-function chkRouter1LowEvent(chk) {
-    routers[0].frequency = Router.LOW_FREQUENCY;
+function chkRouterLowEvent(index) {
+    routers[index].frequency = Router.LOW_FREQUENCY;
 }
 
-function chkRouter1HighEvent(chk) {
-    routers[0].frequency = Router.HIGH_FREQUENCY;
+function chkRouterHighEvent(index) {
+    routers[index].frequency = Router.HIGH_FREQUENCY;
 }
 
-function chkRouter1CustomEvent(chk) {
-    routers[0].frequency = document.getElementById("txtRouter1CustomValue").value;
+function chkRouterCustomEvent(index) {
+    routers[index].frequency = float(routerSettings[index].customFreqSlider.value);
 }
 
-function txtRouter1CustomValueEvent(txt) {
-    if (document.getElementById("chkRouter1Custom").checked)
-        if (!isNaN(float(txt.value)))
-            routers[0].frequency = float(txt.value);
+function sldRouterCustomValueEvent(sld, index) {
+    if (routerSettings[index].customFreqRadio.checked)
+        routers[index].frequency = float(sld.value);
+    routerSettings[index].customFreqOut.value = float(sld.value).toFixed(2);
 }
 
-function sldRouter1PowerEvent(sld) {
-    routers[0].powerPct = float(sld.value);
-    document.getElementById("outRouter1Power").value = float(sld.value).toFixed(2);
+function sldRouterPowerEvent(sld, index) {
+    routers[index].powerPct = float(sld.value);
+    routerSettings[index].powerOut.value = float(sld.value).toFixed(2);
 }
 
 function chkSignOnlyEvent(chk) {
     Room.showSignOnly = chk.checked;
 }
 
-function chkRouter2EnabledEvent(chk) {
-    routers[1].enabled = chk.checked;
+function numDampingEvent(num) {
+    if (!isNaN(float(num.value)))
+        Physics.damping = float(num.value);
 }
 
-function chkRouter2LowEvent(chk) {
-    routers[1].frequency = Router.LOW_FREQUENCY;
+function numPhaseVelocityEvent(num) {
+    if (!isNaN(float(num.value)))
+        Physics.phaseVelocity = float(num.value);
 }
 
-function chkRouter2HighEvent(chk) {
-    routers[1].frequency = Router.HIGH_FREQUENCY;
-}
-
-function chkRouter2CustomEvent(chk) {
-    routers[1].frequency = document.getElementById("txtRouter2CustomValue").value;
-}
-
-function txtRouter2CustomValueEvent(txt) {
-    if (document.getElementById("chkRouter2Custom").checked)
-        if (!isNaN(float(txt.value)))
-            routers[1].frequency = float(txt.value);
-}
-
-function sldRouter2PowerEvent(sld) {
-    routers[1].powerPct = float(sld.value);
-    document.getElementById("outRouter2Power").value = float(sld.value).toFixed(2);
-}
-
-function txtDampingEvent(txt) {
-    if (!isNaN(float(txt.value)))
-        Physics.damping = float(txt.value);
-}
-
-function txtPhaseVelocityEvent(txt) {
-    if (!isNaN(float(txt.value))) {
-        Physics.phaseVelocity = float(txt.value);
-    }
-}
-
-function txtRefractionModifierEvent(txt) {
-    if (!isNaN(float(txt.value)))
-        Physics.globalRefractionModifier = float(txt.value);
+function numRefractionModifierEvent(num) {
+    if (!isNaN(float(num.value)))
+        Physics.globalRefractionModifier = float(num.value);
 }
 
 function sldTicksPerSecondEvent(sld) {
-    Physics.tps = sld.value;
-    document.getElementById("outTicksPerSecond").value = sld.value;
+    Physics.tps = int(sld.value);
+    tpsOut.value = int(sld.value).toString();
 }
 
 function sldStepsPerTickEvent(sld) {
-    Physics.stepsPerTick = sld.value;
-    document.getElementById("outStepsPerTick").value = sld.value;
+    Physics.stepsPerTick = int(sld.value);
+    stepsPerTickOut.value = int(sld.value).toString();
 }
 
 function clrPositiveWave(clr) {
